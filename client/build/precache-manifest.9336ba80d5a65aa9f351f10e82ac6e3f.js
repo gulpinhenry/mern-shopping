4fae3c4a09ebba49bdffd0552670b60954864764
Henry Kam <henry.j.kam@gmail.com>
2021-12-09 01:20:08 -0800

    configure heroku

---
+++ client/build/precache-manifest.9336ba80d5a65aa9f351f10e82ac6e3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e404be5f283fbefa7b584b84bf3d6808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5834c07d4672dd5d4506",
+    "url": "/static/css/main.5a3df583.chunk.css"
+  },
+  {
+    "revision": "767629f919b327bd61b7",
+    "url": "/static/js/2.3495853e.chunk.js"
+  },
+  {
+    "revision": "d4c38a552d93485bc68448a8329e1024",
+    "url": "/static/js/2.3495853e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5834c07d4672dd5d4506",
+    "url": "/static/js/main.416903ef.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
